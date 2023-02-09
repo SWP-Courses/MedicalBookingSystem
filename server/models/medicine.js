e4bd91f3faa9f6944fe05e22f549e59343d74af5
Vu Anh Lam <vuanhlam000@gmail.com>
2023-02-09 13:59:08 +0700

    update

--- server/models/medicine.js
+++ server/models/medicine.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-const medicineSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    maxLength: 255,
-    required: [true, "Please add the medicine name"],
+const medicineSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      maxLength: 255,
+      required: [true, "Please add the medicine name"],
+    },
+    dosageForm: {
+      type: String,
+      required: [true, "Please add the medicine dosage form"],
+    },
+    type: {
+      type: String,
+      maxLength: 255,
+      required: [true, "Please add the medicine type"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please add the medicine price"],
+    },
   },
-  dosageForm: {
-    type: String,
-    required: [true, "Please add the medicine dosage form"],
-  },
-  type: {
-    type: String,
-    maxLength: 255,
-    required: [true, "Please add the medicine type"],
-  },
-  price: {
-    type: Number,
-    required: [true, "Please add the medicine price"],
-  },
-}, {versionKey: false});
+  { versionKey: false }
+);
 
-module.exports = mongoose.model('Medicine', medicineSchema);
+module.exports = mongoose.model("Medicine", medicineSchema);
