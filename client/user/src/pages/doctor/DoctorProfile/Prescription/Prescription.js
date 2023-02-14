55ed0021bb500505578988c777abb1cbaf682746
Vu Anh Lam <vuanhlam000@gmail.com>
2023-02-14 12:52:59 +0700

    prescription logic temp

--- client/user/src/pages/doctor/DoctorProfile/Prescription/Prescription.js
+++ client/user/src/pages/doctor/DoctorProfile/Prescription/Prescription.js
@@ -1,73 +1,130 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Prescription.scss";
 import Select, { colourOptions } from "react-select";
 import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
 import {
   faCirclePlus,
-  faNotesMedical,
   faTrashCan,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { v4 as uuidv4 } from 'uuid';
-import _ from 'lodash'
+import { v4 as uuidv4 } from "uuid";
+import _ from "lodash";
+import axios from "axios";
+import API_URL from "~/api/Router";
 
-function Prescription(props) {
-  //select
-  const [isClearable, setIsClearable] = useState(true);
-  const [isSearchable, setIsSearchable] = useState(true);
-  const [isDisabled, setIsDisabled] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isRtl, setIsRtl] = useState(false);
-
-  // modal
-  const [modalShow, setModalShow] = useState(false);
-  const [smShow, setSmShow] = useState(false);
-
-  // build list Drug
+function Prescription() {
+  const [drugsOption, setDrugsOption] = useState([]);
+  const [desease, setDesease] = useState('');
+  const [note, setNote] = useState('');
+  const [reExamDate, setReExamDate] = useState('');
   const [drugs, setDrugs] = useState([
     {
       id: uuidv4(),
+      drugId: "",
       drugName: "",
       dosage: "",
       quantity: "",
     },
   ]);
 
+  const optionListUsers = [
+    {
+      value: 1,
+      label: "An",
+    },
+    {
+      value: 2,
+      label: "Bình",
+    },
+    {
+      value: 3,
+      label: "Cường",
+    },
+    {
+      value: 4,
+      label: "Dũng",
+    },
+  ];
 
-  const handleAddNewDrug = () => {
-    console.log('handle add new drug');
-    const newDrug = {
+  useEffect(() => {
+    try {
+      fetchAllMedicine();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const fetchAllMedicine = async () => {
+    let res = await axios.get(`${API_URL}/medicine`);
+    if (res && res.data && res.data.medicines) {
+      const listMedicine = res?.data?.medicines?.map((medicine) => {
+        return {
+          value: medicine._id,
+          label: medicine.name,
+        };
+      });
+      setDrugsOption(listMedicine);
+    }
+  };
+
+  const handleOnChangeValue = (type, id, valueSelected) => {
+    const cloneDrugs = _.clone(drugs);
+    const newDrug = cloneDrugs.find((item) => item.id === id);
+    switch (type) {
+      case "drug":
+        if (newDrug) {
+          newDrug.drugId = valueSelected.value;    // valueSelected.value --> id of drug
+          newDrug.drugName = valueSelected.label;  // valueSelected.label --> name of drug
+        }
+        setDrugs(cloneDrugs);
+        break;
+      case "dosage":
+        if (newDrug) {
+          newDrug.dosage = valueSelected;
+        }
+        setDrugs(cloneDrugs);
+        break;
+      case "quantity":
+        if (newDrug) {
+          newDrug.quantity = valueSelected;
+        }
+        setDrugs(cloneDrugs);
+        break;
+      default:
+        throw new Error("on change value error");
+    }
+  };
+
+  const handleAddNewEmptyDrug = () => {
+    const newEmptyDrug = {
       id: uuidv4(),
+      drugId: "",
       drugName: "",
       dosage: "",
       quantity: "",
     };
-    setDrugs([...drugs, newDrug]);
+    setDrugs([...drugs, newEmptyDrug]);
   };
 
   const handleDeleteDrug = (id) => {
-    // setSmShow(true)
-    console.log('drug id: ', id);
-    if(id) {
+    if (id) {
       const drugList = _.clone(drugs);
       const newDrugList = drugList.filter((item) => item.id !== id);
-      if(newDrugList) {
+      if (newDrugList.length > 0) {
         setDrugs(newDrugList);
       }
     }
-  }
-
-  const handleSubmitPrescription = () => {
-    alert("demo");
   };
 
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
+  const handleSubmitPrescription = () => {
+    const prescription = {
+      medicine: drugs,
+      note: note,
+      desease: desease,
+      re_exam_date: reExamDate,
+    }
+    console.log('check final data: ', prescription);
+  };
 
   return (
     <div className="wrapper-prescription">
@@ -77,13 +134,8 @@ function Prescription(props) {
             placeholder={<div>Tìm Bệnh Nhân</div>}
             className="basic-single"
             classNamePrefix="select"
-            isDisabled={isDisabled}
-            isLoading={isLoading}
-            isClearable={isClearable}
-            isRtl={isRtl}
-            isSearchable={isSearchable}
             name="color"
-            options={options}
+            options={optionListUsers}
           />
         </div>
         <div className="user-detail">
@@ -111,15 +163,14 @@ function Prescription(props) {
                 <div className="drug">
                   <Select
                     placeholder={<div>Chọn Thuốc</div>}
+                    defaultValue={drug.drugName}
                     className="basic-single"
                     classNamePrefix="select"
-                    isDisabled={isDisabled}
-                    isLoading={isLoading}
-                    isClearable={isClearable}
-                    isRtl={isRtl}
-                    isSearchable={isSearchable}
                     name="color"
-                    options={options}
+                    options={drugsOption}
+                    onChange={(valueSelected) =>
+                      handleOnChangeValue("drug", drug.id, valueSelected)
+                    }
                   />
                 </div>
                 <div className="qty">
@@ -127,7 +178,14 @@ function Prescription(props) {
                     className="mb-3"
                     controlId="exampleForm.ControlInput1"
                   >
-                    <Form.Control type="text" placeholder="Liều Dùng" />
+                    <Form.Control
+                      type="text"
+                      placeholder="Liều Dùng"
+                      value={drug.dosage}
+                      onChange={(e) =>
+                        handleOnChangeValue("dosage", drug.id, e.target.value)
+                      }
+                    />
                   </Form.Group>
                 </div>
                 <div className="qty">
@@ -135,23 +193,31 @@ function Prescription(props) {
                     className="mb-3"
                     controlId="exampleForm.ControlInput1"
                   >
-                    <Form.Control type="text" placeholder="số lượng" />
+                    <Form.Control
+                      type="text"
+                      placeholder="số lượng"
+                      value={drug.quantity}
+                      onChange={(e) =>
+                        handleOnChangeValue("quantity", drug.id, e.target.value)
+                      }
+                    />
                   </Form.Group>
                 </div>
                 <div className="actions">
                   <div className="delete-btn">
-                    <span 
-                      className="note-icon" 
-                      onClick={() => handleDeleteDrug(drug.id)}
-
-                    >
-                      <FontAwesomeIcon icon={faTrashCan} />
-                    </span>
+                    {drugs && drugs.length > 1 && (
+                      <span
+                        className="note-icon"
+                        onClick={() => handleDeleteDrug(drug.id)}
+                      >
+                        <FontAwesomeIcon icon={faTrashCan} />
+                      </span>
+                    )}
                   </div>
                   <div className="add-btn">
-                    <span 
+                    <span
                       className="note-icon"
-                      onClick={() => handleAddNewDrug()}
+                      onClick={() => handleAddNewEmptyDrug()}
                     >
                       <FontAwesomeIcon icon={faCirclePlus} />
                     </span>
@@ -160,79 +226,54 @@ function Prescription(props) {
               </div>
             ))}
         </div>
-
         <div className="medicine-note">
-          <center>
-            <h5>
-              Thêm nhắc nhở
-            </h5>
-          </center>
-          <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Nhắc Tại đây">
-            {/* At w3schools.com you will learn how to make a website. They offer
-            free tutorials in all web development technologies. */}
-          </textarea>
-          <div className="recall-date">
-            <span>Hẹn Ngày Tái Khám</span>
-            <input type='date'/>
+          <div className="desease-note">
+            <center>
+              <h5>Bệnh</h5>
+            </center>
+            <textarea
+              id="w3review"
+              name="w3review"
+              rows="4"
+              cols="50"
+              placeholder="Loại Bệnh"
+              value={desease} 
+              onChange={e => setDesease(e.target.value)}
+            />
+          </div>
+          <div className="mt-3">
+            <center>
+              <h5>Lưu ý</h5>
+            </center>
+            <textarea
+              id="w3review"
+              name="w3review"
+              rows="4"
+              cols="50"
+              placeholder="Thêm Lưu ý"
+              value={note} 
+              onChange={e => setNote(e.target.value)}
+            />
+            <div className="recall-date">
+              <span>Hẹn Ngày Tái Khám</span>
+              <input 
+                type="date" 
+                value={reExamDate}
+                onChange={(e) => setReExamDate(e.target.value)}
+              />
+            </div>
           </div>
         </div>
       </div>
-      
-      <footer className="">
 
+      <footer className="">
         <button
           className="mt-3 btn-prescription"
           onClick={() => handleSubmitPrescription()}
         >
           Tạo Đơn Thuốc
         </button>
-
-        
       </footer>
-      {/* <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={modalShow}
-      >
-        <Modal.Header closeButton onClick={() => setModalShow(false)}>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Thêm Nhắc Nhở
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Nội Dung</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => setModalShow(false)}>Thêm</Button>
-        </Modal.Footer>
-      </Modal> */}
-      <Modal
-        {...props}
-        size="sm"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={smShow}
-        className="modal-Delete-drug"
-      >
-        <Modal.Header closeButton onClick={() => setSmShow(false)}>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Xóa Thuốc Này
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <center>
-            <Button className="mr-5" variant="danger">
-              Xóa
-            </Button>{" "}
-            <Button variant="primary">Hủy</Button>{" "}
-          </center>
-        </Modal.Body>
-      </Modal>
     </div>
   );
 }
