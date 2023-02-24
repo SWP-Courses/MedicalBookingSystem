ad1a6f32b9d8f6745c6b0fdb14cc0d63bcb0da06
Vu Anh Lam <vuanhlam000@gmail.com>
2023-02-24 10:14:17 +0700

    Set color using variable

--- client/user/src/pages/doctor/DoctorProfile/DoctorSchedule/DoctorSchedule.js
+++ client/user/src/pages/doctor/DoctorProfile/DoctorSchedule/DoctorSchedule.js
@@ -1,24 +1,35 @@
-import "./DoctorSchedule.scss";
-
-import "react-calendar/dist/Calendar.css";
 import Calendar from "react-calendar";
-import { useState } from "react";
+import { useState , useEffect, useContext} from "react";
+import { AuthContext } from "~/context/authContext";
+import axios from "axios";
+
+import "./DoctorSchedule.scss";
+import "react-calendar/dist/Calendar.css";
+import API_URL from "~/api/Router";
 
 function DoctorSchedule() {
-  const [value, onChange] = useState(new Date(2017, 10, 1));
+  const [value, setDate] = useState("");
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {}, []);
+
+  const fetchSchedule = () => {
+    const res = axios.get(`${API_URL}/bookedservices/doctors/${currentUser._id}?date=${value}`);
+  };
 
   return (
-    <div className="wrapper">
-        <div className="calender">
-            <Calendar 
-                onChange={onChange} 
-                value={value} 
-                onClick={(value) => alert('New date is: ', value)}
-            />
-        </div>
-        <div className="doctor-schedule-detail">
-            <h1>doctor schedule detail</h1>
-        </div>
+    <div className="schedule">
+      <div className="schedule__calender">
+        <Calendar
+          value={value}
+          onChange={(value) => setDate(value)}
+        />
+      </div>
+      <div className="schedule__calender-detail">
+        <span>Ngày: </span>
+        <span>{value.toLocaleString()}</span>
+      </div>
     </div>
   );
 }
