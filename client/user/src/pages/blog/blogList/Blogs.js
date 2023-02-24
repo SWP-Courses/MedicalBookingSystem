6ece5f69b00a255e6f338ad22ca4cced1fb07fd1
Vu Anh Lam <vuanhlam000@gmail.com>
2023-02-24 20:44:19 +0700

    Handle error when call API

--- client/user/src/pages/blog/blogList/Blogs.js
+++ client/user/src/pages/blog/blogList/Blogs.js
@@ -7,6 +7,7 @@ import API_URL from "~/api/Router";
 import ReactPaginate from "react-paginate";
 import slider from "~/assets/images/slider.jpg";
 import Pagination from "./Pagination";
+import { hanlderRequest } from "~/utils";
 
 function Blogs() {
   const listBlogRef = useRef();
@@ -21,19 +22,21 @@ function Blogs() {
   }, []);
 
   const fetchListCategoryBlogs = async () => {
-    const res = await axios.get(API_URL + "/category");
-    // console.log('check res cate: ', res);
+    const [error, res] = await hanlderRequest(axios.get(API_URL + "/category"));
     if (res && res.data && res.data.category && res.data.category.length > 0) {
       setBlogCategory(res.data.category);
+    } else {
+      console.log(error.messsage);
     }
   };
 
   const fetchBlogs = async () => {
-    const res = await axios.get(API_URL + "/blogs");
-    // console.log('check res blog: ', res);
+    const [error, res] = await hanlderRequest(axios.get(API_URL + "/blogs"));
     if (res && res.data && res.data.blogs && res.data.blogs.length > 0) {
       listBlogRef.current = res.data.blogs;
       setBlogs(res.data.blogs);
+    }else {
+      console.log(error);
     }
   };
 
@@ -75,7 +78,7 @@ function Blogs() {
           <div className="blog-list">
             <h1 className="type">{categoryName}</h1>
             <hr />
-            <Pagination data={blogs}/>
+            <Pagination data={blogs} />
           </div>
         </div>
         <div className="paginate">
--- client/user/src/utils.js
+++ client/user/src/utils.js
@@ -11,7 +11,6 @@ const shortenText = (text, limit) => {
   return trimmedString;
 };
 
-
 const validateEmail = (email) => {
   return String(email)
     .toLowerCase()
@@ -20,41 +19,45 @@ const validateEmail = (email) => {
     );
 };
 
-
 const checkStringContainInPhoneNumber = (phone) => {
   var Regex = /\b[\+]?[(]?[0-9]{2,6}[)]?[-\s\.]?[-\s\/\.0-9]{3,15}\b/m;
-  return !Regex.test(phone)
-}
+  return !Regex.test(phone);
+};
 
 function validateUsername(username) {
   const pattern = /^[\p{L}\s]+$/u;
   return pattern.test(username);
 }
 
-const convertServiceName = (serviceId, services) =>{
-  
-  const service = services.find(service => service._id === serviceId)
-return service.name
-}
+const convertServiceName = (serviceId, services) => {
+  const service = services.find((service) => service._id === serviceId);
+  return service.name;
+};
 
-const convertDoctorName = (serviceId, services) =>{
-  
-  const service = services.find(service => service._id === serviceId)
-return service.name
-}
+const convertDoctorName = (serviceId, services) => {
+  const service = services.find((service) => service._id === serviceId);
+  return service.name;
+};
 
 const formatDate = (value) => {
   const year = value.getFullYear();
-  const month = (value.getMonth() + 1).toString().padStart(2, '0');
-  const day = value.getDate().toString().padStart(2, '0');
+  const month = (value.getMonth() + 1).toString().padStart(2, "0");
+  const day = value.getDate().toString().padStart(2, "0");
   const formattedDate = `${day}-${month}-${year}`;
   return formattedDate;
-}
+};
+
+const hanlderRequest = (promise) => {
+  return promise
+          .then((data) => [undefined, data])
+          .catch((error) => [error, undefined]);
+};
 
 export {
   shortenText,
   validateEmail,
   checkStringContainInPhoneNumber,
   validateUsername,
-  formatDate
-}
+  formatDate,
+  hanlderRequest
+};
